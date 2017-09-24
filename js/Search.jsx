1c9e93bc6089e247ac3bbd9503936d2d1b45ada8
Taki Vlasakakis <takivlasakakis@Takis-MacBook-Pro.local>
2017-09-23 19:53:11 -0600

    @4:25

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import ShowCard from "./ShowCard";
+import preload from "../data.json";
 
-const Search = () => <h1>hi lol this is search </h1>;
+const Search = () => (
+	<div className="search">
+		<div>
+			{preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+		</div>
+	</div>
+);
 
 export default Search;
